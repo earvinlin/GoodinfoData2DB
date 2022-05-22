2fd653dab1c67c478ea6fa9c289e0ea4d273e6ba
earvinlin.02(imac) <earvinlin.02@gmail.com>
2022-05-22 14:38:14 +0800

    20220522-1437 調整資料來源為指定之路徑並批次產生sql command

--- FormatStocksSalemonData.py
+++ FormatStocksSalemonData.py
@@ -1,5 +1,5 @@
 """
-20220521-2049 (Not Finish)調整資料來源為…
+20220521-2049 調整資料來源為指定之路徑並批次產生sql command
 """
 from datetime import datetime
 import sys
@@ -8,6 +8,7 @@ import time
 import openpyxl
 import time
 import platform
+import re
 
 NULL_VALUE = "null"
 insertCnt = 0 
@@ -15,88 +16,95 @@ isFirstLine = True
 try:
 	print("[FormatFile.py] 開始執行時間：" + time.strftime('%Y-%m-%d %H:%M:%S',time.localtime()))
 
-	if len(sys.argv) < 3 :
-		print("You need input one parameter(fmt : 資料抓取日期)")
+	if len(sys.argv) < 2 :
+		print("You need input one parameter(資料日期, fmt : yyyymmdd)")
 		print("syntax(windows)    : C:\python GetGoodinfoSalemonData.py 20220517")
 		print("syntax(imac/linux) : $python3 GetGoodinfoSalemonData.py 20220517")
 		sys.exit()
 
 	theDate = sys.argv[1]
 
-	loadFileDir = "Data\\EXCEL\\Transfer\\salemon\\"
+#	loadFileDir = "Data\\EXCEL\\Transfer\\salemon\\"
 #	loadFileDir = "C:\Users\linea\OneDrive\myStocksPGMs\V2.0\xls2xlsx\transfer_ok\saleMonth\\"
-	saveFileDir = "Data\\TXT\\salemon\\"
+#	saveFileDir = "Data\\TXT\\salemon\\"
 
 	# 設定檔案存取路徑
-	loadFileDir = os.path.join("Data", "EXCEL", "Transfer", "salemon", str(theDate))
-	saveFileDir =  os.path.join("Data", "TXT", "salemon", str(theDate))
+	loadFileDir = ""
+	saveFileDir = ""
 	if platform.system() == "Windows" :
-	   	loadFileDir += "\\"
-		saveFileDir += "\\"
+		loadFileDir = "Data\\EXCEL\\Transfer\\salemon\\" + str(theDate) + "\\"
+		saveFileDir = "Data\\TXT\\salemon\\" + str(theDate) + "\\"
 	else :
-		loadFileDir += "/"
-		saveFileDir += "/"
+		loadFileDir = "./Data/EXCEL/Transfer/salemon/" + str(theDate) + "/"
+		saveFileDir = "./Data/TXT/salemon/" + str(theDate) + "/"
+
 	print("loadFile dir: " + loadFileDir)
 	print("saveFile dir: " + saveFileDir)
 
-#	inputFile = stockCode + "-salemon-" + sys.argv[2] + ".xlsx"
-#	outputFile = stockCode + ".txt"
-	
-	outfile = open(saveFileDir + outputFile, 'w')
+	# 20220522 取得要處理的檔案資料
+	files = os.listdir(loadFileDir)
+	# 以迴圈處理
+	for inputFile in files:
+		relativePath = os.path.join(loadFileDir, inputFile)
+		if os.path.isfile(relativePath) :
+			print("檔案：", inputFile)
 
-#	wb = openpyxl.load_workbook('csv\\goodinfo\\saleMonth\\' + inputFile)
-	print(loadFileDir + inputFile)
-	wb = openpyxl.load_workbook(loadFileDir + inputFile)
-	sheet = wb.worksheets[0]
+		stockCode = inputFile[:4]
+		outputFile = stockCode + ".txt"
+		print("outfile: " + outputFile)
+		outfile = open(saveFileDir + outputFile, 'w')
 
-	isSTOP = False
-	irow = 5
-	icol = 1
+#		wb = openpyxl.load_workbook('csv\\goodinfo\\saleMonth\\' + inputFile)
+		print(loadFileDir + inputFile)
+		wb = openpyxl.load_workbook(loadFileDir + inputFile)
+		sheet = wb.worksheets[0]
+
+		isSTOP = False
+		irow = 5
+		icol = 1
 
 #	theList.append("insert into stocks_sale_month (stock_no,date,open_price_mon,end_price_mon,hgh_price_mon,low_price_mon,ups_downs,ups_downs_p,mon_revenue,mon_increase_in_revenue,ann_ins_revenue,cum_revenue,ann_ins_cum_revenue_p,csd_revenue,mon_ins_csd_revenue_p,ann_ins_csd_revenue_p,cum_csd_revenue,ann_ins_cum_csd_revenue_p) values ('" + stockCode + "'")
 #	theSQLCmd = "insert into stocks_sale_month (stock_no, date, open_price_mon, end_price_mon, hgh_price_mon, low_price_mon, ups_downs, ups_downs_p, mon_revenue, mon_increase_in_revenue, ann_ins_revenue, cum_revenue, ann_ins_cum_revenue_p, csd_revenue, mon_ins_csd_revenue_p, ann_ins_csd_revenue_p, cum_csd_revenue, ann_ins_cum_csd_revenue_p) values ('%s', '%d', '%f',  '%f',  '%f',  '%f',  '%f',  '%f',  '%f', '%f', '%f', '%f', '%f', '%f', '%f', '%f', '%f', '%f')"
 #	outfile.write(theSQLCmd+"\n")
-	while not isSTOP :
-		theList = []
-		theList.append("insert into stocks_sale_month (stock_no,date,open_price_mon,end_price_mon,hgh_price_mon,low_price_mon,ups_downs,ups_downs_p,mon_revenue,mon_increase_in_revenue,ann_ins_revenue,cum_revenue,ann_ins_cum_revenue_p,csd_revenue,mon_ins_csd_revenue_p,ann_ins_csd_revenue_p,cum_csd_revenue,ann_ins_cum_csd_revenue_p) values ('" + stockCode + "'")
+		while not isSTOP :
+			theList = []
+			theList.append("insert into stocks_sale_month (stock_no,date,open_price_mon,end_price_mon,hgh_price_mon,low_price_mon,ups_downs,ups_downs_p,mon_revenue,mon_increase_in_revenue,ann_ins_revenue,cum_revenue,ann_ins_cum_revenue_p,csd_revenue,mon_ins_csd_revenue_p,ann_ins_csd_revenue_p,cum_csd_revenue,ann_ins_cum_csd_revenue_p) values ('" + stockCode + "'")
 
-		for icol in range(1, 18) :
-			theValue = sheet.cell(row = irow, column = icol).value
+			for icol in range(1, 18) :
+				theValue = sheet.cell(row = irow, column = icol).value
 #			print(theValue)
 #			if sheet.cell(row = irow, column = icol).value == None :
-			if icol == 1 and sheet.cell(row = irow, column = icol).value == None :
+				if icol == 1 and sheet.cell(row = irow, column = icol).value == None :
 					isSTOP = True
 					theList.pop()
 					break
-			elif type(theValue) == str :
-				if theValue == "-" :
-					theValue = NULL_VALUE
-				theList.append(theValue)
-			elif type(theValue) == int :
-				theList.append(theValue)
-			elif type(theValue) == float :
-				theList.append(theValue)
-			elif type(theValue) == datetime :
-				theList.append(str(theValue.strftime("%Y%m%d")))
-			else :
-				theList.append(theValue)
-		print(theList)
-		if len(theList) > 0 :
-			outfile.write(", ".join([str(_) for _ in theList]))
-			outfile.write(");\n")
-		insertCnt += 1
+				elif type(theValue) == str :
+					if theValue == "-" :
+						theValue = NULL_VALUE
+					theList.append(theValue)
+				elif type(theValue) == int :
+					theList.append(theValue)
+				elif type(theValue) == float :
+					theList.append(theValue)
+				elif type(theValue) == datetime :
+					theList.append(str(theValue.strftime("%Y%m%d")))
+				else :
+					theList.append(theValue)
+#			print(theList)
+			if len(theList) > 0 :
+				outfile.write(", ".join([str(_) for _ in theList]))
+				outfile.write(");\n")
+			insertCnt += 1
 
-		# 預防錯誤，理論上應該不會超過5000列(1年12筆，100年1200筆；所以約400年的企業才可能有問題)
-		if irow > 5000 :
-			print("i=" + str(irow))
-			isSTOP = True
-		irow += 1
+			# 預防錯誤，理論上應該不會超過5000列(1年12筆，100年1200筆；所以約400年的企業才可能有問題)
+			if irow > 5000 :
+				print("i=" + str(irow))
+				isSTOP = True
+			irow += 1
 
-	outfile.close()
-	
+		outfile.close()
+		print("資料處理完成!! 共 " + str(insertCnt) + " 筆。")
+		print("[FormatFile.py] 結束執行時間：" + time.strftime('%Y-%m-%d %H:%M:%S',time.localtime()))
+
 except IOError as err :
 	print('File error : ' + str(err))
-
-print("資料處理完成!! 共 " + str(insertCnt) + " 筆。")
-print("[FormatFile.py] 結束執行時間：" + time.strftime('%Y-%m-%d %H:%M:%S',time.localtime()))
-
