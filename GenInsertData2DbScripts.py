7fbc310a558edd49d449c478877090372776d3c4
earvinlin(off_vmwin8) <lin.earvin@gmail.com(off_vmwin8)>
2022-05-23 13:42:48 +0800

    20220523-1342 新增產生執行新增資料庫之scripscript.

--- GenInsertData2DbScripts.py
+++ GenInsertData2DbScripts.py
@@ -1,32 +1,74 @@
 """
 20220522 (撰寫中) 產生執行InsertData2DbByFile.py的script
-         需要輸入的參數：檔案路徑 執行的作業系統(Windows / Linux / iMac) 
+         需要輸入的參數：檔案路徑 
          -------------------------------------------------------------------------
          Content :
          -------------------------------------------------------------------------
          <dividend> -- Windows (Linux/iMac -> python3 ...) --
-         python InsertData2DbByFile.py Data\TXT\dividend\22020520\ 2002.txt
+         python InsertData2DbByFile.py Data\TXT\dividend\22020520\ 
          ...
 
          <salemon> -- Windows (Linux/iMac -> python3 ...) --
-         python InsertData2DbByFile.py Data\TXT\salemon\22020520\ 2002.txt
+         python InsertData2DbByFile.py Data\TXT\salemon\22020520\ 
          ...
 
          <BzPerformance> -- Windows (Linux/iMac -> python3 ...) --
-         python InsertData2DbByFile.py Data\TXT\BzPerformance\22020520\ 2002.txt
+         python InsertData2DbByFile.py Data\TXT\BzPerformance\22020520\ 
          ...
 """
-import mysql.connector
 import sys
 import os 
 import time
+import platform
 
+print("[GenInsertData2DbScripts.py] 開始執行時間：" + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
 
-print("[InsertData2DbByFile.py] 開始執行時間：" + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
-
-insertCnt = 0
+loadFileDir = ""
+outputFile = ""
+processCnt = 0
 errorCnt = 0
+try:
+	print("[FormatFile.py] 開始執行時間：" + time.strftime('%Y-%m-%d %H:%M:%S',time.localtime()))
 
+	if len(sys.argv) < 2 :
+		print("You need input one parameter : 檔案目錄")
+		print("syntax(windows)    : C:\python GenInsertData2DbScripts.py Data\\TXT\\alemon\\0220522\\")
+		print("syntax(imac/linux) : $python3 GenInsertData2DbScripts.py Data\\TXT\\salemon\\20220522\\")
+		sys.exit()
 
-print("資料處理完成!! 共 " + str(insertCnt) + " 筆；錯誤 " + str(errorCnt) + " 筆。")
-print("[InsertStocksFromPolarisToMySQLDB.py] 結束執行時間：" + time.strftime('%Y-%m-%d %H:%M:%S',time.localtime()))
+	# 設定檔案存取路徑
+	pythonCompiler = ""
+	if platform.system() == "Windows" :
+		pythonCompiler = "python"
+		loadFileDir = sys.argv[1]
+		outputFile = "_InsertStocksData2DB.Bat"
+	else :
+		pythonCompiler = "python3"
+		loadFileDir = sys.argv[1]
+		outputFile = "_InsertStocksData2DB.sh"
+	
+	outfile = open(outputFile, 'w')
+
+    # python InsertData2DbByFile.py Data\TXT\salemon\20220522\ 1101.txt
+
+	# 20220522 取得要處理的檔案資料
+	files = os.listdir(loadFileDir)
+	# 以迴圈處理
+	for inputFile in files:
+		relativePath = os.path.join(loadFileDir, inputFile)
+		if os.path.isfile(relativePath) :
+			print("檔案：", inputFile)
+
+		stockCode = inputFile[:4]
+		writeContent = pythonCompiler + " InsertData2DbByFile.py " + loadFileDir + " " + stockCode + ".txt"
+#		print("Content: " + writeContent)
+		outfile.write(writeContent + "\n")
+		processCnt += 1
+
+	outfile.close()
+
+	print("資料處理完成!! 共 " + str(processCnt) + " 筆。")
+	print("[GenInsertData2DbScripts.py] 結束執行時間：" + time.strftime('%Y-%m-%d %H:%M:%S',time.localtime()))
+
+except IOError as err :
+	print('File error : ' + str(err))
